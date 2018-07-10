ad12b8afd86da588fdffedaf98b93905d3f9f4d3
Rolf Pedersen <rolf.pedersen@spk.no>
2018-07-10 12:59:55 +0200

    Example: Delete branch on remote repo.

--- cli-examples.sh
+++ cli-examples.sh
@@ -1,4 +1,7 @@
+# Git
+# ----------
 # Create a tarball from Git repo. Use --prefix to get a top-level folder when extracting
-git archive -o archive_name.tar.gz --prefix=repo_name/ HEAD
-
+git archive -o archive_name.tar.gz --prefix=repo_name/ HEAD 
+# Delete branch on remote repo origin
+git push --delete origin branch_name
 
