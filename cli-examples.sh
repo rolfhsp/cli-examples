1e2815a68a128aef87510c66d2be87a517a17dec
Rolf Pedersen <rolfhsp@gmail.com>
2025-02-18 10:31:07 +0100

    Update cli-examples.sh
    Heading change...

--- cli-examples.sh
+++ cli-examples.sh
@@ -1,5 +1,5 @@
-# Git
-# ----------
+# Git - Useful commands
+# ---------------------
 # Create a tarball from Git repo. Use --prefix to get a top-level folder when extracting
 git archive -o archive_name.tar.gz --prefix=repo_name/ HEAD 
 # Delete branch on remote repo origin
